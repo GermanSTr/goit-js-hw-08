f21fe0ec9edcd1902fe543a6dc8b1b90b76a1a0c
GermanSTr <gershev1994@gmail.com>
2023-10-26 16:29:40 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+
+function renderImg() {
+  const galleryHTML = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
+  });
+  galleryEl.innerHTML = galleryHTML.join('');
+}
+renderImg();
+
+var lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
