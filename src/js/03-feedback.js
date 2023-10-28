e06e32dc031cb70a64c75dc6e80615fa05b84dc7
GermanSTr <gershev1994@gmail.com>
2023-10-28 14:46:59 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,18 @@
-const test = 'hello';
-console.log(test);
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const KEY_LOCAL_FORM = 'feedback-form-state';
+
+form.addEventListener('input', infoInput);
+
+function infoInput() {
+  const formData = {
+    name: form.querySelector('[name="email"]').value,
+    email: form.querySelector('[name="message"]').value,
+  };
+  localStorage.setItem(KEY_LOCAL_FORM, JSON.stringify(formData));
+}
+
+if (localStorage.getItemtItem(KEY_LOCAL_FORM) !== null) {
+  localStorage.setItem(KEY_LOCAL_FORM, JSON.stringify(formData));
+}
